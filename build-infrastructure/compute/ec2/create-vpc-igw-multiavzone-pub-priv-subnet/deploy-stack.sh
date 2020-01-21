4be4deca0ce157e5dd0fac2a545b78a810906a58
Ian Wallace <iwallace1975@gmail.com>
2020-01-21 18:38:44 +0000

    Add create-vpc-igw-multiavzone-pub-priv-subnet

---
+++ build-infrastructure/compute/ec2/create-vpc-igw-multiavzone-pub-priv-subnet/deploy-stack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+STACK_NAME=$1
+
+if [ -z "$1" ]
+  then
+    echo "No STACK_NAME argument supplied"
+    exit 1
+fi
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Creating stack..."
+STACK_ID=$( \
+  aws cloudformation create-stack \
+  --stack-name ${STACK_NAME} \
+  --template-body file://${DIR}/create-stack.yaml \
+  --parameters file://${DIR}/create-stack-parameters.json \
+  | jq -r .StackId \
+)
+
+echo "Waiting on ${STACK_ID} create completion..."
+aws cloudformation wait stack-create-complete --stack-name ${STACK_ID}
+aws cloudformation describe-stacks --stack-name ${STACK_ID} | jq .Stacks[0].Outputs
